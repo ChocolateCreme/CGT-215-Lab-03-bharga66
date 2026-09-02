70bbf56967f05b7d40bafe0a706e2a08bd187b6e
Avani <bhargava.avani25@gmail.com>
2026-09-02 14:41:02 -0400

    Final Solution

--- CGT-215-Lab-03-bharga66/CGT-215-Lab-03-bharga66.cpp
+++ CGT-215-Lab-03-bharga66/CGT-215-Lab-03-bharga66.cpp
@@ -5,23 +5,21 @@
 using namespace std;
 void printMenu(int& choice) {
 	cout << "Welcome to my program, please select an operation to perform:" << endl;
-	cout << "1. Placeholder" << endl;
-	cout << "2. Another Placeholder" << endl;
-	cout << "3. And a third one" << endl;
-	cout << "\nYour Selection: ";
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "Your Choice: ";
 	cin >> choice;
 }
 void getChoices(float& A, float& B) {
-	cout << "Please enter the first value:";
+	cout << "Please enter the first value (A): ";
 	cin >> A;
+	cout << "Please enter the second value (B): ";
+	cin >> B;
 	// The rest of this function is an exercise to the reader
 }
-void firstChoice(float A, float B) {
-	cout << "This is proving the first choice ran" << endl;
-}
-void secondChoice(float A, float B) {
-	cout << "This is proving the second choice ran" << endl;
-}
+
 int main() {
 	int choice;
 	float A;
@@ -29,10 +27,16 @@ int main() {
 	printMenu(choice);
 	getChoices(A, B);
 	if (choice == 1) {
-		firstChoice(A, B);
+		cout << "The addition of A and B is " << A + B;
 	}
 	if (choice == 2) {
-		secondChoice(A, B);
+		cout << "The subtraction of A and B is " << A - B;
+	}
+	if (choice == 3) {
+		cout << "The multiplication of A and B is " << A * B;
+	}
+	if (choice == 4) {
+		cout << "The division of A and B is " << A / B;
 	}
 	return 0;
 }
